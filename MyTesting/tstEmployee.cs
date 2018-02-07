ae5c9775d70de3b924a38be1483a6ab26adddd58
p16224700 <p16224700@GWHXX282WIN017W>
2018-02-07 10:23:38 +0000

    testing

---
+++ MyTesting/tstEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClassLibrary;
+
+namespace MyTesting
+{
+    [TestClass]
+    public class tstEmployee
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create a new instance of the class we want to create
+            clsEmployee AnEmployee = new clsEmployee();
+            //test to see if it exists
+            Assert.IsNotNull(AnEmployee);
+        }
+    }
+}
