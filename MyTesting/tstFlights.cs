a8262b4d126c04401c789c077e20b2963c50fa69
p16222545 <p16222545@GWHXX651WIN116W>
2018-02-12 12:14:14 +0000

    test

---
+++ MyTesting/tstFlights.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClassLibrary;
+
+namespace MyTesting
+{
+    [TestClass]
+    public class tstFlights
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create a new instance of the class we want to create
+            clsFlights AnFlight = new clsFlights();
+            //test to see if it exists
+            Assert.IsNotNull(AnFlight);
+        }
+    }
+}
