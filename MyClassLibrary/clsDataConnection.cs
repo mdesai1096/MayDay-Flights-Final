46b364ec619a32a56e4ea86dfb1ca72e4e177268
Mohammad Desai <mdesai1096@gmail.com>
2018-02-20 13:51:21 +0000

    fixes

--- MyClassLibrary/clsDataConnection.cs
+++ MyClassLibrary/clsDataConnection.cs
@@ -27,7 +27,7 @@ public class clsDataConnection
 
     public clsDataConnection()
     {
-        GetConString("C:\\Users\\mdesa\\Google Drive\\Uni Work\\MayDay-Flights-TDD-Coding-SQL-Presentation\\App_Data\\Mayday Flights.mdf");
+        GetConString("C:\\Users\\mdesa\\Source\\Repos\\MayDay-Flights-TDD-C-SQL-ASP\\App_Data\\Mayday Flights.mdf");
    
     }
 
