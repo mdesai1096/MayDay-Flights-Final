8db6f44758546fb5a16c537c9d44c0e968070bb7
Mohammad Desai <mdesai1096@gmail.com>
2018-02-20 13:21:21 +0000

    conflicts fixsd

---
+++ MyTesting/tstCustomerCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClassLibrary;
+using System.Collections.Generic;
+using Class_Library;
+
+namespace MyTesting
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomerCollection AnCustomer = new clsCustomerCollection();
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
+    
