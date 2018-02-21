321341608413c1e6988ef9eb013fb3a910835e3d
p16224700 <p16224700@my365.dmu.ac.uk>
2018-02-21 10:54:24 +0000

    Hi i did some contact number and address :D

--- MyClassLibrary/clsEmployee.cs
+++ MyClassLibrary/clsEmployee.cs
@@ -12,5 +12,29 @@ namespace MyClassLibrary
         public string EmployeeName { get; set; }
         public string EmployeeRole { get; set; }
         public string EmployeeSalary { get; set; }
+
+        public string Valid(string employeeAddress, string employeeContactNumber, string employeeDOB, string employeeEmail, string employeeJoinDate, string employeeName, string employeeRole, string employeeSalary)
+        {
+            string Error = "";
+            if (employeeAddress.Length == 0)
+            {
+                Error = Error + "The employee address may bot be blank : ";
+            }
+            if (employeeAddress.Length > 30)
+            {
+                Error = Error + "The employee address must be 30 characters or less : ";
+            }
+            if (employeeAddress.Length > 90)
+            {
+                Error = Error + "The employee address must be less than 90 characters : ";
+            }
+            if (employeeContactNumber.Length == 0)
+            {
+                Error = Error + "Contact number can't be wrong ";
+            }
+            return Error;
+        }
+
+        
     }
 }
