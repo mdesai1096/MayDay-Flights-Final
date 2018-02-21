020b35afc076d0711a0c3f093cd8f6ae4903df9e
ayub <ayubosman@hotmail.co.uk>
2018-02-21 18:38:56 +0000

    changes to tstcust + clscust

--- MyClassLibrary/clsCustomer.cs
+++ MyClassLibrary/clsCustomer.cs
@@ -12,25 +12,137 @@ namespace MyClassLibrary
         public string Date { get; set; }
         public string PostCode { get; set; }
 
-        public bool Valid(string postCode, string date)
+        public string Valid(string password, Action username, string contactNo, string customerID, Action email, string flatNo, string forename, Action gender, string surname, string postCode, string v, string dateT)
         {
             DateTime DateTemp;
-            Boolean OK = false;
-            if (postCode.Length < 20)
+            string OK = "";
+            if (postCode.Length < 0)
             {
-                OK = false;
+                OK = OK + "fhfhf";
             }
             else
             {
-                OK = true;
+                OK = OK + "fhfhf";
             }
-            DateTemp = Convert.ToDateTime(date);
+            DateTemp = Convert.ToDateTime(dateT);
             if (DateTemp > DateTime.Now.Date)
             {
 
-                OK = false;
+                OK = OK + "fhfhf";
             }
             return OK;
         }
+
+        public string Valid(string postCode, string date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string postCode, string surname, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string forename, Action gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string email, string flatNo, string forename, Action gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string username, string contactNo, string customerID, Action email, string flatNo, string forename, Action gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string contactNo, string customerID, Action email, string flatNo, string forename, Action gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string flatNo, string forename, Action gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(bool gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string customerID, Action email, string flatNo, string forename, Action gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string flatNo, string forename, string gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string email, string flatNo, string forename, string gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string contactNo, string customerID, Action email, string flatNo, string forename, string gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string username, string contactNo, string customerID, Action email, string flatNo, string forename, string gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string forename, string gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string password, Action username, string contactNo, string customerID, Action email, string flatNo, string forename, string gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string customerID, Action email, string flatNo, string forename, string gender, string surname, string postCode, string v, string dateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public bool Valid(string postCode, string date)
+        //{
+        //    DateTime DateTemp;
+        //    Boolean OK = false;
+        //    if (postCode.Length < 0)
+        //    {
+        //        OK = false;
+        //    }
+        //    else
+        //    {
+        //        OK = true;
+        //    }
+        //    DateTemp = Convert.ToDateTime(date);
+        //    if (DateTemp > DateTime.Now.Date)
+        //    {
+
+        //        OK = false;
+        //    }
+        //    return OK;
+
+        //}
     }
 }
