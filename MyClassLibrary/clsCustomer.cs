639bb465ea88714dc04e73f73ad361bbd6d6267d
Ayub Osman <33282130+ayubosman123@users.noreply.github.com>
2018-03-14 10:33:17 +0000

    Add files via upload

--- MyClassLibrary/clsCustomer.cs
+++ MyClassLibrary/clsCustomer.cs
@@ -9,42 +9,191 @@ namespace MyClassLibrary
         {
         }
 
-        public string ContactNumber { get; set; }
-        public string dateOfBirth { get; set; }
-        public string EMail { get; set; }
-        public string FlatNo { get; set; }
-        public string ForeName { get; set; }
-        public string Gender { get; set; }
-        public string HouseNo { get; set; }
-        public string Password { get; set; }
-        public string PostCode { get; set; }
-        public string Surname { get; set; }
-        public string UserName { get; set; }
+        private string mContactNumber;
+        public string ContactNumber {
+            get
+            {
+                return mContactNumber;
+            }
+            set
+            {
+                mContactNumber = value;
+            }
 
-        public string Valid(string userName, string password, string foreName, string surname, string dateOfBirth, string gender, string contactNumber, string eMail, string flatNo, string houseNo, string postCode)
+        }
+
+
+        private DateTime mdateOfBirth;
+        public DateTime dateOfBirth {
+            get
+            {
+                return mdateOfBirth;
+            }
+            set
+            {
+                mdateOfBirth = value;
+            }
+
+        }
+
+        private string mEmail;
+        public string EMail {
+
+            get
+            {
+                return mEmail;
+            }
+            set
+            {
+                mEmail = value;
+            }
+
+        }
+
+
+        private string mFlatNo;
+        public string FlatNo {
+              get
+            {
+                return mFlatNo;
+            }
+    set
+            {
+                mFlatNo = value;
+            }
+
+        }
+
+        private string mForeName;
+        public string ForeName {
+            get
+            {
+                return mForeName;
+            }
+            set
+            {
+                mForeName = value;
+            }
+
+        }
+
+
+        private string mGender;
+        public string Gender {
+            get
+            {
+                return mGender;
+            }
+            set
+            {
+                mGender = value;
+            }
+
+        }
+
+        private string mHouseNo;
+        public string HouseNo {
+            get
+            {
+                return mHouseNo;
+            }
+            set
+            {
+                mHouseNo = value;
+            }
+
+        }
+
+        private string mPassword;
+        public string Password {
+            get
+            {
+                return mPassword;
+            }
+            set
+            {
+                mPassword = value;
+            }
+
+        }
+
+        private string mPostCode;
+        public string PostCode {
+            get
+            {
+                return mPostCode;
+            }
+            set
+            {
+                mPostCode = value;
+            }
+
+        }
+
+        private string mSurname;
+        public string Surname {
+            get
+            {
+                return mSurname;
+            }
+            set
+            {
+                mSurname = value;
+            }
+
+        }
+
+        private string mUserName;
+        public string UserName {
+            get
+            {
+                return mUserName;
+            }
+            set
+            {
+                mUserName = value;
+            }
+
+        }
+
+        private Int32 mCustomerID;
+        public Int32 CustomerID
+        {
+            get
+            {
+                return mCustomerID;
+            }
+            set
+            {
+                mCustomerID = value;
+            }
+
+        }
+
+        public string Valid( string foreName, string surname, string dateOfBirth, string gender, string contactNumber, string eMail, string flatNo, string houseNo, string postCode)
         {
             DateTime DateTemp;
             string OK = "";
 
-            if (userName.Length < 5)
-            {
-                OK = OK + "Username is too short";
-            }
-            if (userName.Length > 20)
-            {
-                OK = OK + "Username is too long";
-            }
+            //if (userName.Length < 5)
+            //{
+            //    OK = OK + "Username is too short";
+            //}
+            //if (userName.Length > 20)
+            //{
+            //    OK = OK + "Username is too long";
+            //}
 
             //////////////////////////////////
 
-            if (password.Length < 5)
-            {
-                OK = OK + "Password is too short";
-            }
-            if (password.Length > 20)
-            {
-                OK = OK + "Password is too long";
-            }
+            //if (password.Length < 5)
+            //{
+            //    OK = OK + "Password is too short";
+            //}
+            //if (password.Length > 20)
+            //{
+            //    OK = OK + "Password is too long";
+            //}
 
             //////////////////////////////////
 
@@ -157,8 +306,36 @@ namespace MyClassLibrary
 
         public bool Find(int CustomerID)
         {
-            //Always return true
-            return true;
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the book ref to search for
+            DB.AddParameter("@CustId", CustomerID);
+            //execute stored procedure
+            DB.Execute("sproc_FiltertblCustomerbyPK");
+            //if one record is found
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mCustomerID = Convert.ToInt32(DB.DataTable.Rows[0]["Customer_ID"]);
+                mSurname = Convert.ToString(DB.DataTable.Rows[0]["surName"]);
+                mForeName = Convert.ToString(DB.DataTable.Rows[0]["Forename"]);
+                mGender = Convert.ToString(DB.DataTable.Rows[0]["Gender"]);
+                mdateOfBirth = Convert.ToDateTime(DB.DataTable.Rows[0]["Date of Birth"]);
+                mPostCode = Convert.ToString(DB.DataTable.Rows[0]["Postcode"]);
+                mEmail = Convert.ToString(DB.DataTable.Rows[0]["Email"]);
+                mHouseNo = Convert.ToString(DB.DataTable.Rows[0]["HouseNo"]);
+                mFlatNo = Convert.ToString(DB.DataTable.Rows[0]["FlatNo"]);
+                mContactNumber = Convert.ToString(DB.DataTable.Rows[0]["ContactNo"]);
+
+                //return an record has been found
+                return true;
+            }
+            //if no record is found
+            else
+            {
+                //record no record has been found
+                return false;
+            }
         }
         }
     }
